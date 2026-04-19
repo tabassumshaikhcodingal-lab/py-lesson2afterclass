c69f98554f6d4ba42a669e12efd37f0b96be25d2
tabassumshaikhcodingal-lab <tabassumshaikh.codingal@gmail.com>
2026-04-19 13:10:57 +0530

    Comment out birthdate input code and add note
    Commented out code for inputting and printing birthdates. Added a note for future implementation.

--- mian.py
+++ mian.py
@@ -1,3 +1,22 @@
 print("Welcome to the party!")
 print("Hello guys\n")
 print("welcome to the party", end= "*")
+
+
+
+/*
+b1 = input("Enter birthdate 1: ")
+b2 = input("Enter birthdate 2: ")
+b3 = input("Enter birthdate 3: ")
+b4 = input("Enter birthdate 4: ")
+b5 = input("Enter birthdate 5: ")
+
+# Printing values
+print("\nStored Birthdates:")
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)*/
+
+You cand add this code you have to accept brithdate and print it
